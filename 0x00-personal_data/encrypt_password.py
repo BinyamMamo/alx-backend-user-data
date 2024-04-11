93d8cb2578e887eefd34a003593b0aca8bd25b9c
BinyamMamo <binyammamo01@gmail.com>
2024-04-11 22:29:00 +0400

    feat: implement logging and database functionality
    - Add get_logger function to configure logging with PII redaction
    - Add get_db function to connect to database
    - Implement main function to log user data from database
    - Remove unused imports, declarations, and main guard condition

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+Task 5 & 6: Encrypting passwords
+Implement a hash_password function that returns a salted, hashed password
+"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Encrypts a password using bcrypt"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    checks if a given password matches a hashed password using the bcrypt
+    algorithm.
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,8 +5,10 @@ Write a function called `filter_datum`
 that returns the log message obfuscated
 """
 import logging
+import os
 import re
 from typing import List
+import mysql.connector
 
 
 class RedactingFormatter(logging.Formatter):
@@ -35,15 +37,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
     Filters personal data from log messages
-
-    Parameters:
-        fields (list): List of field names to redact
-        redaction (str): String to replace redacted data with
-        message (str): Log message to filter
-        separator (str): Separator between field name and value
-
-    Returns:
-        str: Filtered log message
     """
     for field in fields:
         message = re.sub(rf"{field}=(.*?)\{separator}",
@@ -51,5 +44,50 @@ def filter_datum(fields: List[str], redaction: str, message: str,
     return message
 
 
-if __name__ == "__main__":
-    pass
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging object configured to
+    log user data with redacted PII fields.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """
+    Get a connection to the database.
+    """
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
+
+
+def main() -> None:
+    """ Implement a main function
+    """
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
