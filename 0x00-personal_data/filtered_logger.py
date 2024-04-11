1e9d9dc6ef1d281a7371c7c7734ec37687a585a6
BinyamMamo <binyammamo01@gmail.com>
2024-04-11 15:31:51 +0400

    refactor: add RedactingFormatter class for logging

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,28 @@ that returns the log message obfuscated
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """
+    Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        """ Initialize the RedactingFormatter object
+        """
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ Format the log record by filtering sensitive data
+        """
+        return filter_datum(self.fields, self.REDACTION,
+                        super().format(record), self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
