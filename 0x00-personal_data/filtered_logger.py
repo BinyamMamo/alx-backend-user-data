20b31b9aaf678176d6b39bbdcdbc81146da8c1da
BinyamMamo <binyammamo01@gmail.com>
2024-04-11 15:33:57 +0400

    refactor: improve imports and add type hints
    - Move logging import above other imports
    - Add List type hint to RedactingFormatter fields parameter
    - Initialize fields attribute in RedactingFormatter **init**

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,9 @@ Task 0. Regex-ing
 Write a function called `filter_datum`
 that returns the log message obfuscated
 """
+import logging
 import re
 from typing import List
-import logging
 
 
 class RedactingFormatter(logging.Formatter):
@@ -18,9 +18,10 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """ Initialize the RedactingFormatter object
         """
+        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
