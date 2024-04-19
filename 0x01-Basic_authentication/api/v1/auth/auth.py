177c6749c54891e0f7b3fd4dbd4ba6926ff2b206
BinyamMamo <binyammamo01@gmail.com>
2024-04-19 11:52:08 +0400

    fix: Implement require_auth to check if path requires authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,25 +2,29 @@
 Module containing authentication classes and functions.
 """
 from flask import request
-from typing import List
+from typing import List, TypeVar
 
 
 class Auth:
     """Auth class for handling authentication."""
-    
-    def __init__(self) -> None:
-        """Initialize the Auth instance."""
-        pass
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Check if the given path requires authentication.
         """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Return the authorization header for the request.
         """
-        return None
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Return the currently authenticated user.
