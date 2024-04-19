6e2644b3ea7bafb71984addc890421461fba5540
BinyamMamo <binyammamo01@gmail.com>
2024-04-19 20:32:17 +0400

    fix: return None if authorization header is invalid format

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -41,6 +41,6 @@ class BasicAuth(Auth):
             self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
         if (not isinstance(decoded_base64_authorization_header, str) or
                 ":" not in decoded_base64_authorization_header):
-            return None, None
+            return (None, None)
 
-        return tuple(decoded_base64_authorization_header.split(":"))
+        return tuple(decoded_base64_authorization_header.split(":", 1))
