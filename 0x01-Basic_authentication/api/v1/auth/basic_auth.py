4ba3fd2f3a9003b80a14a0eecca26a88d5c9bc78
BinyamMamo <binyammamo01@gmail.com>
2024-04-19 20:22:41 +0400

    feat: add extract_user_credentials method and Tuple import

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,7 @@ basic_auth.py
 """
 from api.v1.auth.auth import Auth
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Tuple
 import base64
 import binascii
 
@@ -36,3 +36,11 @@ class BasicAuth(Auth):
                 return res.decode("utf-8")
             except (binascii.Error, UnicodeDecodeError):
                 return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> Tuple[str, str]:
+        if (not isinstance(decoded_base64_authorization_header, str) or
+                ":" not in decoded_base64_authorization_header):
+            return None, None
+
+        return tuple(decoded_base64_authorization_header.split(":"))
