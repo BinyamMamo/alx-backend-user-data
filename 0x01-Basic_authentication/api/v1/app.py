c87841985194eeb9bee6ae31104cb9ca9ebefdc1
BinyamMamo <binyammamo01@gmail.com>
2024-04-19 13:52:55 +0400

    fix: invert auth check in before_request

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -46,7 +46,7 @@ def before_request():
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
                       '/api/v1/forbidden/']
-    if auth.require_auth(request.path, excluded_paths) is None:
+    if not auth.require_auth(request.path, excluded_paths):
         return
 
     if auth.authorization_header(request) is None:
