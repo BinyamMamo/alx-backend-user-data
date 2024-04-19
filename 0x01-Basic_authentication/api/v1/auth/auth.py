c9504cc24fa287e77db109608407b355d27c5fcd
BinyamMamo <binyammamo01@gmail.com>
2024-04-19 22:46:16 +0400

    fix: handle wildcard paths in excluded_paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -16,10 +16,19 @@ class Auth:
         """
         if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
+
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith("*"):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+
         if path[-1] != "/":
             path += "/"
         if path in excluded_paths:
             return False
+
         return True
 
     def authorization_header(self, request=None) -> str:
